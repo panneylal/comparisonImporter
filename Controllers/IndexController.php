0f5c15a607320afd10e144cfa26ba253f788dace
mplociennik <mplociennik@users.noreply.github.com>
2015-08-18 14:42:39 +0200

    Create IndexController.php

---
+++ Controllers/IndexController.php
@@ -0,0 +1,11 @@
+class ComparisonImporter_IndexController extends Mage_Core_Controller_Front_Action
+{
+   public function indexAction ()
+   {
+     echo 'test index';
+   }
+   public function mamethodeAction ()
+   {
+     echo 'test mymethod';
+    }
+}
