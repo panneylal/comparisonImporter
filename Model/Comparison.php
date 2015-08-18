43432f3c693b8aa749eafa4c4addb212120a14bb
cieniu <cieniuu@gmail.com>
2015-08-18 23:39:14 +0200

    Poprawiona całość i dodane funkcje

--- Model/Comparison.php
+++ Model/Comparison.php
@@ -1,15 +1,33 @@
 <?php
 
-class Comparison{
+openlog("comparisonLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
-    // method declaration
-    public function displayRecords($xml) {
+class Comparison{
+    
+    // Get xml.
+    public function getRecords($xml) {
     $results = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_COMPACT | LIBXML_PARSEHUGE)or die("Error: Cannot create object");
     
         return $results;
+		syslog(LOG_INFO, "Get products: END");
     }
 
+	public function save($data){
+		
+		/* Tu się zapisze mi do bazy :] */
+		
+		syslog(LOG_INFO, "Data saved!");
+	}
+	
+	public function startParsing($urlFile){
+        
+		$xmltoparse = file_gets_contents($urlFile);
+		save($xmltoparse);
+		syslog(LOG_INFO, "Save records: END");
+		
+    }
 
 }
 
+closelog();
 ?>
