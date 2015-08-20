fc86e81a51c1287ac92ab8a13b2f653e3a3f73e5
mplociennik <mplociennik@users.noreply.github.com>
2015-08-20 09:06:06 +0200

    Create IndexController.php

---
+++ modulefiles/app/code/local/Pfay/Test/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,110 @@
+<?php
+class Pfay_Test_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+    protected function _initAction()
+    {
+        $this->loadLayout()->_setActiveMenu('test/set_time')
+                ->_addBreadcrumb('test Manager','test Manager');
+       return $this;
+     }
+      public function indexAction()
+      {
+         $this->_initAction();
+         $this->renderLayout();
+      }
+      public function editAction()
+      {
+           $testId = $this->getRequest()->getParam('id');
+           $testModel = Mage::getModel('test/test')->load($testId);
+           if ($testModel->getId() || $testId == 0)
+           {
+             Mage::register('test_data', $testModel);
+             $this->loadLayout();
+             $this->_setActiveMenu('test/set_time');
+             $this->_addBreadcrumb('test Manager', 'test Manager');
+             $this->_addBreadcrumb('Test Description', 'Test Description');
+             $this->getLayout()->getBlock('head')
+                  ->setCanLoadExtJs(true);
+             $this->_addContent($this->getLayout()
+                  ->createBlock('test/adminhtml_test_edit'))
+                  ->_addLeft($this->getLayout()
+                  ->createBlock('test/adminhtml_test_edit_tabs')
+              );
+             $this->renderLayout();
+           }
+           else
+           {
+                 Mage::getSingleton('adminhtml/session')
+                       ->addError('Test does not exist');
+                 $this->_redirect('*/*/');
+            }
+       }
+       public function newAction()
+       {
+          $this->_forward('edit');
+       }
+       public function saveAction()
+       {
+         if ($this->getRequest()->getPost())
+         {
+           try {
+                 $postData = $this->getRequest()->getPost();
+                 $testModel = Mage::getModel('test/test');
+               if( $this->getRequest()->getParam('id') <= 0 )
+                  $testModel->setCreatedTime(
+                     Mage::getSingleton('core/date')
+                            ->gmtDate()
+                    );
+                  $testModel
+                    ->addData($postData)
+                    ->setUpdateTime(
+                             Mage::getSingleton('core/date')
+                             ->gmtDate())
+                    ->setId($this->getRequest()->getParam('id'))
+                    ->save();
+                 Mage::getSingleton('adminhtml/session')
+                               ->addSuccess('successfully saved');
+                 Mage::getSingleton('adminhtml/session')
+                                ->settestData(false);
+                 $this->_redirect('*/*/');
+                return;
+          } catch (Exception $e){
+                Mage::getSingleton('adminhtml/session')
+                                  ->addError($e->getMessage());
+                Mage::getSingleton('adminhtml/session')
+                 ->settestData($this->getRequest()
+                                    ->getPost()
+                );
+                $this->_redirect('*/*/edit',
+                            array('id' => $this->getRequest()
+                                                ->getParam('id')));
+                return;
+                }
+              }
+              $this->_redirect('*/*/');
+            }
+          public function deleteAction()
+          {
+              if($this->getRequest()->getParam('id') > 0)
+              {
+                try
+                {
+                    $testModel = Mage::getModel('test/test');
+                    $testModel->setId($this->getRequest()
+                                        ->getParam('id'))
+                              ->delete();
+                    Mage::getSingleton('adminhtml/session')
+                               ->addSuccess('successfully deleted');
+                    $this->_redirect('*/*/');
+                 }
+                 catch (Exception $e)
+                  {
+                           Mage::getSingleton('adminhtml/session')
+                                ->addError($e->getMessage());
+                           $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+                  }
+             }
+            $this->_redirect('*/*/');
+       }
+}
+?>
